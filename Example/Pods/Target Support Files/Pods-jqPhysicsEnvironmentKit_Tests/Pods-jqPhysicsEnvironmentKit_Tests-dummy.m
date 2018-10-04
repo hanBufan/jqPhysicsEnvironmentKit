1b84779aa8afa93775ea3073de3685936013403f
jeffesonQin <1247006353@qq.com>
2018-10-04 22:47:50 +0800

    Create the Physics Environment Project via pod create.

---
+++ Example/Pods/Target Support Files/Pods-jqPhysicsEnvironmentKit_Tests/Pods-jqPhysicsEnvironmentKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_jqPhysicsEnvironmentKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_jqPhysicsEnvironmentKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/jqPhysicsEnvironmentKit/jqPhysicsEnvironmentKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_jqPhysicsEnvironmentKit : NSObject
+@end
+@implementation PodsDummy_jqPhysicsEnvironmentKit
+@end
